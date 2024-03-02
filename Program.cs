3013d6b111a81fac866519b471abd491b88e019d
HoStWgSp <hostwg@vk.com>
2024-03-02 17:39:23 +1100

    Ура, я инициализировал переменную.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EducationProcess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Evgeniy";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
