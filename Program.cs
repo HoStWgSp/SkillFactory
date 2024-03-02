fbd186b7071b548a9521100c42cd9e471525a21e
HoStWgSp <hostwg@vk.com>
2024-03-02 18:36:37 +1100

    Перечисления

--- Program.cs
+++ Program.cs
@@ -4,20 +4,19 @@
     {
         static void Main(string[] args)
         {
-            string MyName = "Evgeniy";
-            byte MyAge = 41;
-            bool HaveIApet = true;
-            double MyShoeSize = 40.5;
+            Semaphore semaphoreLight;
 
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("MyAge " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+            semaphoreLight = Semaphore.GREEN;
 
-            Console.WriteLine("IntMin {0} ", int.MinValue);
-            Console.WriteLine("IntMax {0} ", int.MaxValue);
-
+            Console.WriteLine(semaphoreLight);
             Console.ReadKey();
+        }
+
+        enum Semaphore
+        {
+            RED = 100,
+            YELLOW = 200,
+            GREEN = 300
         }
     }
 }
