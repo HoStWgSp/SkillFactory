1d5e4edfcfff9ac470ed0baa9d0502746d4a1aa6
HoStWgSp <hostwg@vk.com>
2024-03-04 10:13:05 +1100

    3.6 Основные операции C#

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
-            string name = "Evgeniy";
-            int age = 41;
-            string favcolor = "black";
-            Console.WriteLine("{0}\n  {1}\n {2}", name, age, favcolor);
+            double result = 5.0 / 2.0;
+            Console.WriteLine("5 / 2 = {0}", result);
+            result = 5.0 % 2.0;
+            Console.WriteLine("5 / 2 = {0}", result);
+            int counter = 10;
+            Console.WriteLine("Value: {0} Increment: {1}", counter, ++counter);
+            counter = 10;
+            Console.WriteLine("Value: {0} Increment: {1}", counter, counter++);
+            result = 10.0 * 2.0 / 5.0;
+            Console.WriteLine("Value: {0}", result);
+            result = 10 % 3;
+            Console.WriteLine("Value: {0}", result);
             Console.ReadKey();
         }
     }
