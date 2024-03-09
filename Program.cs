e6f59219fece663afcb07538a7d2f7b29c267dff
HoStWgSp <hostwg@vk.com>
2024-03-09 17:02:28 +1100

    4.3 Массивы 4.3.17

--- Program.cs
+++ Program.cs
@@ -9,17 +9,27 @@ namespace EducationProcess
         {
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
             var numbers = 0;
-            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            int numvar;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
             {
-                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
                 {
-                    if (arr[i,j] > 0)
-                        numbers++;
+                    
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    {
+                        if (arr[i, j] > arr[i, k])
+                        {
+                            numvar = arr[i, j];
+                            arr[i, j] = arr[i, k];
+                            arr[i, k] = numvar;
+                        }
+                    }
+                    Console.Write(arr[i, j]);
+
                 }
-                
+                Console.WriteLine();
             }
 
-            Console.WriteLine(numbers);
             Console.ReadKey();
         }
     }
