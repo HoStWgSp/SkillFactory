4bc9cf2733e94cdddda109180674dba558689e67
HoStWgSp <hostwg@vk.com>
2024-03-09 16:11:57 +1100

    4.3 Массивы 4.3.7

--- Program.cs
+++ Program.cs
@@ -14,14 +14,10 @@ namespace EducationProcess
 
             Console.WriteLine("Ваше имя по буквам: ");
 
-            foreach (var ch in name)
+            for (int i = name.Length - 1; i >= 0; i--)
             {
-                Console.Write(ch + " ");
-            }
-
-            Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
-
-           
+                Console.Write(name[i] + " ");
+            }         
 
 
             Console.ReadKey();
