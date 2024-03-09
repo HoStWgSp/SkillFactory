9b7ed55ad5140cf35a6afc3f0afa13b05551e15e
HoStWgSp <hostwg@vk.com>
2024-03-09 14:36:06 +1100

    4.1. Условные выражения и конструкции 4.1.16

--- Program.cs
+++ Program.cs
@@ -6,71 +6,32 @@ namespace EducationProcess
     {
         static void Main(string[] args)
         {
-            //int olddata = 6;
-            //byte data = (byte)olddata;
-            //Console.WriteLine(data);
-            //Console.WriteLine(olddata);
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-            //olddata = 6;
-            //string data = olddata.ToString();
-            //Console.WriteLine(data);
-            //Console.WriteLine(olddata);
+            var color = Console.ReadLine();
 
-            //Console.Write("Enter your age: ");
-            //int age = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Your age is {0} ", age);
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-            //Console.Write("Enter your age: ");
-            //int age = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Your age is {0} ", age);
+                Console.WriteLine("Your color is red!");
+            }
 
-            //Console.Write("Enter your age: ");
-            //int age;
-            //bool iscorrect = int.TryParse(Console.ReadLine(), out age);
-            //Console.WriteLine("Your age is {0} ", age);
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-            //Console.Write("Enter your name: ");
-            //string name = Console.ReadLine();
-            //Console.Write("Enter your age: ");
-            //byte age = (byte)int.Parse(Console.ReadLine());
-            //Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-            //Console.Write("Enter your name: ");
-            //string name = Console.ReadLine();
-            //Console.Write("Enter your age: ");
-            //byte age = checked((byte)int.Parse(Console.ReadLine()));
-            //Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-
-            //Console.Write("What is your favorite day of week? ");
-            //DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
-            //Console.WriteLine("Your favorite day is {0}", day);
-
-            //Console.Write("Enter your name: ");
-            //var name = Console.ReadLine();
-            //Console.Write("Enter your age: ");
-            //var age = checked((byte)int.Parse(Console.ReadLine()));
-            //Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-            //Console.Write("What is your favorite day of week? ");
-            //var day = (DayOfWeek)int.Parse(Console.ReadLine());
-            //Console.WriteLine("Your favorite day is {0}", day);
-            //var howItall = 162;
-            //var myshoe = 37.5;
-            //Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-            //Console.WriteLine("What is my grouth? {0} sm", howItall);
-            //Console.WriteLine("What is my shoe size? {0}", myshoe);
-
-            Console.Write("Enter your name: ");
-            var name = Console.ReadLine();
-
-            Console.Write("Enter your age: ");
-            var age = checked((byte)int.Parse(Console.ReadLine()));
-            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-
-            Console.Write("What is your favorite day of week? ");
-
-            var day = (DayOfWeek)int.Parse(Console.ReadLine());
-            Console.WriteLine("Your favorite day is {0}", day);
-
+                Console.WriteLine("Your color is cyan!");
+            }
             Console.ReadKey();
         }
     }
