635b396a0f2f77d9d93287f56dc4684a210acdfd
HoStWgSp <hostwg@vk.com>
2024-03-09 16:06:46 +1100

    4.3 Массивы 4.3.5

--- Program.cs
+++ Program.cs
@@ -8,45 +8,20 @@ namespace EducationProcess
         static void Main(string[] args)
         {
 
-            string[] favcolors = new string[3];
+            Console.WriteLine("Введите своё имя");
 
-            for (int i = 0; i < favcolors.Length; i++)
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по буквам: ");
+
+            foreach (var ch in name)
             {
-                Console.WriteLine("Введите любимый цвет номер {0}", i + 1);
-                favcolors[i] = Console.ReadLine();
+                Console.Write(ch + " ");
             }
 
-            foreach (var color in favcolors)
-                switch (color)
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
 
-                        Console.WriteLine("Your color is red!");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-                        break;
-
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-                        break;
-
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Your color is yellow!");
-                        break;
-                }
+           
 
 
             Console.ReadKey();
