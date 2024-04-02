d91f652091eadb1747df8342c118f0ea03bd687b
HoStWgSp <hostwg@vk.com>
2024-04-02 18:59:48 +1100

    Задание 7.2.3

--- Program.cs
+++ Program.cs
@@ -18,28 +18,17 @@ namespace EducationProcess
         }
         class BaseClass
         {
-            protected string Name;
-
-            public BaseClass(string name)
+            public virtual void Display()
             {
-                Name = name;
+                Console.WriteLine("Метод класса BaseClass");
             }
         }
 
         class DerivedClass : BaseClass
         {
-            public string Description;
-
-            public int Counter;
-
-            public DerivedClass(string name, string description):base(name)
+            public override void Display()
             {
-                Description = description;
-            }
-            public DerivedClass(string name, string description, int counter) : base(name)
-            {
-                Description = description;
-                Counter = counter;
+                Console.WriteLine("Метод класса DerivedClass");
             }
         }
     }
