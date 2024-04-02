690768c6b92baf8a173abe16db5f5f6cc3ba3984
HoStWgSp <hostwg@vk.com>
2024-04-03 03:31:20 +1100

    Задание 7.6.2

--- Program.cs
+++ Program.cs
@@ -14,33 +14,20 @@ namespace EducationProcess
         
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
-
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
-
-            Console.ReadKey();
-
-            
+            Console.ReadKey();            
         }
     }
-    static class IntExtention
+    
+    class Car<T>
     {
-        public static int GetPositive(this int num)
-        {
-            if (num < 0) return -num;
-            else return num;
-        }
-        public static int GetNegative(this int num)
-        {
-            if (num > 0) return -num;
-            else return num;
-        }
+        public T Engine;
+    }
+    class ElectricEngine
+    {
+
+    }
+    class GasEngine
+    {
+
     }
 }
