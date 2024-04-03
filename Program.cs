b411153cceaa4abeca159bad6523e423180abcb8
HoStWgSp <hostwg@vk.com>
2024-04-03 19:04:17 +1100

    7.7. Итоги
    Уже написал на форум, что не могу писать код без конечной цели, так как не могу сам себе придумывать задания и выполнять, я так пробовал учиться, так не работает. Самообучение зацикливается на стадии придумывания себе задания. Так что, что смог выдавить, то выдавил.
    
    Пожалуйста сделайте задания:
    1. Входные данные
    2. Логика обработки
    3. Выходные данные
    
    Что бы научиться как можно эффективнее достигать конечного результата программируя на данном языке.
    
    Как я понял мы изучаем язык и реализацию кода (техническую сторону), а не дизайн. Или я, что то путаю???

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Text.RegularExpressions;
@@ -14,44 +15,102 @@ namespace EducationProcess
         
         static void Main(string[] args)
         {
+            Order<HomeDelivery> order = new Order<HomeDelivery>();
+            order.Customer = new HomeDelivery()
+            {
+                Name = "Толян",
+                LastName = "Прохоров",
+                Address = "Красная поляна, д.9",
+                PhoneNumber = "89621356879",
+                CarNumber = "А596РЕ99",
+                DriverName = "Алексей куравлев"
+            };
+
+            Product product = new Product(20, "20x50x30", Color.Blue);
+            product.DisplayProduct();
+            order.DisplayOrder();
+            order.Customer.DeliveryDetails();
+
             Console.ReadKey();            
         }
     }
-   abstract class Engine
+    abstract class Customer
     {
-
+        public string Name {  get; set; }
+        public string LastName {  get; set; }
+        public string PhoneNumber {  get; set; }
+        public string Address {  get; set; }
     }
-    class ElectricEngine : Engine { }
 
-    class GasEngine : Engine { }
-
-    abstract class CarPart { }
-
-    class Battery : CarPart { }
-
-    class Differential : CarPart { }
-
-    class Wheel : CarPart { }
-
-    abstract class Car<TEngine> where TEngine : Engine
+    class HomeDelivery : Customer
     {
-        public TEngine Engine;
+        public string CarNumber {  get; set; }
+        public string DriverName {  get; set; }
 
-        public abstract void ChangePart<TPart>(TPart newPart) where TPart : CarPart;
-    }
-    class GasCar : Car<ElectricEngine>
-    {
-        public override void ChangePart<TPart>(TPart newPart)
+        public void DeliveryDetails()
         {
-            
+            Console.WriteLine("\nДетали доставки:");
+            Console.WriteLine("Номер машины доставки: {0}\nИмя водителя - {1}", CarNumber, DriverName);
+        }
+    }
+
+    class PickPointDelivery : Customer
+    {
+        public string PickPointName {  get; set; }
+        public string PickPointAddress { get; set; }
+        public void DeliveryDetails()
+        {
+            Console.WriteLine("\nДетали доставки:");
+            Console.WriteLine("Название точки получения: {0}\nАдрес точки получателя - {1}", PickPointName, PickPointAddress);
+        }
+    }
+
+    class ShopDelivery : Customer
+    {
+        public string ShopName { get; set; }
+        public string ShopAddress { get; set; }
+        public void DeliveryDetails()
+        {
+            Console.WriteLine("\nДетали доставки:");
+            Console.WriteLine("Название магазина: {0}\nАдрес магазина - {1}", ShopName, ShopAddress);
+        }
+    }
+
+    class Product
+    {
+        public int Weight {  get; set; }
+        public string Size { get; set; }
+        public Color Colour { get; set; } = Color.Blue;
+
+        public Product(int weight, string size, Color colour)
+        {
+            Weight = weight;
+            Size = size;
+            Colour = colour;
+        }
+
+        public void DisplayProduct()
+        {
+            Console.WriteLine("Товар:\nВес {0} кг.\nРазмер {1} см.\nРасцветка товара - {2}", Weight, Size, Colour.Name);
         }
 
     }
-    class ElectricCar : Car<GasEngine>
-    {
-        public override void ChangePart<TPart>(TPart newPart)
-        {
 
+    
+    
+    class Order<TCustomer> where TCustomer : Customer
+    {
+        public TCustomer Customer;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayOrder()
+        {
+            Console.WriteLine("\nЗаказчик {0} {1}",Customer.LastName, Customer.Name);
+            Console.WriteLine("Телефон заказчика: {0}",Customer.PhoneNumber);
+            Console.WriteLine("Адрес заказчика: {0}",Customer.Address);
         }
     }
 }
