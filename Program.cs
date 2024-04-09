e01c2d7817a3ce6c28f33dc5eca38eb831fe441d
HoStWgSp <hostwg@vk.com>
2024-04-09 13:06:35 +1100

    Задание 8.3.2

--- Program.cs
+++ Program.cs
@@ -21,15 +21,50 @@ namespace EducationProcess
         }
         public static void ProgInfo()
         {
-            string filePath = @"C:\Users\gridar\Documents\Projects\VisualStudio\SkillFactory\EducationProcess\Program.cs";
+            string tempFile = Path.GetTempFileName(); // используем генерацию имени файла.
+            var fileInfo = new FileInfo(tempFile); // Создаем объект класса FileInfo.
 
-            using (StreamReader sr = new StreamReader(filePath))
+
+            //fileInfo.CreationTime = DateTime.Now;
+            //Console.WriteLine("Время открытия файла - {0}", fileInfo.CreationTime);
+
+            //Создаем файл и записываем в него.
+            using (StreamWriter sw = fileInfo.CreateText())
+            {
+                sw.WriteLine("Врем запуска {0}",DateTime.Now);
+                sw.WriteLine("Игорь");
+                sw.WriteLine("Андрей");
+                sw.WriteLine("Сергей");
+            }
+
+            //Открываем файл и читаем из него.
+            using (StreamReader sr = fileInfo.OpenText())
             {
                 string str = "";
                 while ((str = sr.ReadLine()) != null)
                 {
                     Console.WriteLine(str);
                 }
+            }
+
+            try
+            {
+                string tempFile2 = Path.GetTempFileName();
+                var fileInfo2 = new FileInfo(tempFile2);
+
+                // Убедимся, что файл назначения точно отсутствует
+                fileInfo2.Delete();
+
+                // Копируем информацию
+                fileInfo.CopyTo(tempFile2);
+                Console.WriteLine($"{tempFile} скопирован в файл {tempFile2}.");
+                //Удаляем ранее созданный файл.
+                fileInfo.Delete();
+                Console.WriteLine($"{tempFile} удален.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e}");
             }
         }
     }
