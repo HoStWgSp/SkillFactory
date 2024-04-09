50be1638684ea30fe5f243768a2c28057168c733
HoStWgSp <hostwg@vk.com>
2024-04-09 13:40:36 +1100

    Задание 8.4.1

---
+++ BinRead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationProcess
+{
+    internal static class BinRead
+    {
+        public static string FileString { get; set; }
+
+        public static void FileInfo(string filePaht)
+        {
+            if (File.Exists(filePaht))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(File.Open(filePaht, FileMode.Open)))
+                {
+                    FileString = binaryReader.ReadString();
+                }
+                Console.WriteLine("Файл содержит строку: {0}", FileString);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,57 +15,10 @@ namespace EducationProcess
         
         static void Main(string[] args)
         {
-            ProgInfo();
+            BinRead.FileInfo(@"C:\Users\gridar\Desktop\BinaryFile.bin");
 
         Console.ReadKey();
         }
-        public static void ProgInfo()
-        {
-            string tempFile = Path.GetTempFileName(); // используем генерацию имени файла.
-            var fileInfo = new FileInfo(tempFile); // Создаем объект класса FileInfo.
-
-
-            //fileInfo.CreationTime = DateTime.Now;
-            //Console.WriteLine("Время открытия файла - {0}", fileInfo.CreationTime);
-
-            //Создаем файл и записываем в него.
-            using (StreamWriter sw = fileInfo.CreateText())
-            {
-                sw.WriteLine("Врем запуска {0}",DateTime.Now);
-                sw.WriteLine("Игорь");
-                sw.WriteLine("Андрей");
-                sw.WriteLine("Сергей");
-            }
-
-            //Открываем файл и читаем из него.
-            using (StreamReader sr = fileInfo.OpenText())
-            {
-                string str = "";
-                while ((str = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(str);
-                }
-            }
-
-            try
-            {
-                string tempFile2 = Path.GetTempFileName();
-                var fileInfo2 = new FileInfo(tempFile2);
-
-                // Убедимся, что файл назначения точно отсутствует
-                fileInfo2.Delete();
-
-                // Копируем информацию
-                fileInfo.CopyTo(tempFile2);
-                Console.WriteLine($"{tempFile} скопирован в файл {tempFile2}.");
-                //Удаляем ранее созданный файл.
-                fileInfo.Delete();
-                Console.WriteLine($"{tempFile} удален.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Ошибка: {e}");
-            }
-        }
+        
     }
 }
