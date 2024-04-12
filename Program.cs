de216eb3bbb0f17e24c4f0d61de2c1f4fca0eb65
HoStWgSp <hostwg@vk.com>
2024-04-12 13:39:04 +1100

    Задание 9.3.2

--- Program.cs
+++ Program.cs
@@ -15,24 +15,21 @@ namespace EducationProcess
 {
     internal class Program
     {
+        delegate int CalculateDelegate(int a, int b);
         static void Main(string[] args)
         {
-            try
-            {
-                throw new RankException("Сообщение об ошибке");
-            }
 
-            catch (RankException ex)
-            {
-                Console.WriteLine(ex.GetType);
-            }
+            CalculateDelegate calcDelegate = Calculate;
+            int result = calcDelegate.Invoke(100, 30);
 
-            finally
-            {
-                Console.Read();
-            }
-
+            Console.WriteLine(result);
             Console.ReadKey();
+
+        }
+
+        static int Calculate(int a, int b)
+        {
+            return a - b;
         }
     }
 }
