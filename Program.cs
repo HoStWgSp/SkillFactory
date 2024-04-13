79500f7437d28b75fd64634368903734b1b3e05a
HoStWgSp <hostwg@vk.com>
2024-04-13 14:43:48 +1100

    Задание 9.4.3

--- Program.cs
+++ Program.cs
@@ -13,26 +13,39 @@ using System.IO;
 
 namespace EducationProcess
 {
-    internal class Program
+    class Program
     {
-        public delegate Car HandlerMethod();
+        public delegate Parent HandlerMethod();
+        delegate void ChildInfo(Child child);
 
-        public static Car CarHandler()
+        public static Parent ParentHandler()
         {
             return null;
         }
-        public static Lexus LexusHandler()
+
+
+        public static Child ChildHandler()
         {
             return null;
         }
 
         static void Main(string[] args)
         {
-            HandlerMethod handlerLexus = LexusHandler;
+            ChildInfo childInfo = GetParentInfo;
 
-            Console.ReadKey();
+            childInfo.Invoke(new Child());
+
+            Console.Read();
+
+        }
+
+        public static void GetParentInfo(Parent p)
+        {
+            Console.WriteLine(p.GetType());
         }
     }
-    class Car { }
-    class Lexus : Car { }
+
+    class Parent { }
+
+    class Child : Parent { }
 }
