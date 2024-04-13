049a4db969250758403b8d44020df5721abd807d
HoStWgSp <hostwg@vk.com>
2024-04-13 13:41:27 +1100

    Задание 9.3.12

--- Program.cs
+++ Program.cs
@@ -15,36 +15,16 @@ namespace EducationProcess
 {
     internal class Program
     {
+        
+        delegate void ShowMessageDelegate(string _message);
         static void Main(string[] args)
         {
-            Action showMessageDelegate = ShowMessage;
-            showMessageDelegate.Invoke();
-
-            Func <int, int, int, int> sumDelegate = Sum;
-            int result = sumDelegate.Invoke(1, 30, 120);
-            Console.WriteLine(result);
-
-            Predicate <string> checkLengthDelegate = CheckLength;
-            bool status = checkLengthDelegate.Invoke("skill_factory");
-            Console.WriteLine(status);
-
-            Console.ReadLine();
-        }
-
-        static void ShowMessage()
-        {
-            Console.WriteLine("Hello World!");
-        }
-
-        static int Sum(int a, int b, int c)
-        {
-            return a + b + c;
-        }
-
-        static bool CheckLength(string _row)
-        {
-            if (_row.Length > 3) return true;
-            return false;
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
         }
     }
 }
