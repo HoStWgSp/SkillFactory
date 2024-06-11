6dc8c298731404872beccc496ba5f6c0b4ac788b
Kuchenin Evgeniy <kuchenin.evgeniy@yandex.ru>
2024-06-11 17:50:58 +1100

    Задание 16.2.4

---
+++ UserTicketService.Test/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserTicketService;
+
+namespace UserTicketService.Test
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+    }
+}
